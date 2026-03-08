568a6a5c0605d0f9c20455c754629e2fb327f6bc
thetzawaung-ton <241034270+thetzawaung-ton@users.noreply.github.com>
2026-03-08 09:48:47 +0630

    Add nav button and blank div

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 import "./styles.css";
-import testImage from "./test.png";
-
-const image = document.createElement("img");
-image.src = testImage;
-document.body.appendChild(image);
 console.log("Hello");
